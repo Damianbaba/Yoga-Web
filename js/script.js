3f3a7d7b565020f243af584ea79566d95c82b3de
Damian Mroczek <damianmroczek@yahoo.com>
2023-04-06 17:12:47 +0700

    section bio

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     const nav = document.querySelector('.navbar')
-
     const allNavItems = document.querySelectorAll('.nav-link')
-
     const navList = document.querySelector('.navbar-collapse')
 
     function addShadow() {
-
         if (window.scrollY >= 300) {
             nav.classList.add('shadow-bg')
         }
@@ -15,9 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
             nav.classList.remove('shadow-bg')
         }
     }
-
     allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
-
     window.addEventListener('scroll', addShadow)
 })
 
