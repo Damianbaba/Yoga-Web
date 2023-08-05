9b43b750659a4e1bac59a109fe5f1d064b0009de
Damian Mroczek <damianmroczek@yahoo.com>
2023-08-05 18:44:22 +0700

    add bio section modal

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const nav = document.querySelector('.navbar')
     const allNavItems = document.querySelectorAll('.nav-link')
     const navList = document.querySelector('.navbar-collapse')
+
+
 
     function addShadow() {
         if (window.scrollY >= 300) {
@@ -14,4 +19,24 @@ document.addEventListener('DOMContentLoaded', function () {
     allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
     window.addEventListener('scroll', addShadow)
 })
+
+document.getElementById("readMoreBtn").addEventListener("click", function () {
+    document.getElementById("bioModal").style.display = "block";
+});
+
+document.getElementById("closeModal").addEventListener("click", function () {
+    document.getElementById("bioModal").style.display = "none";
+});
+
+document.getElementById("exitModal").addEventListener("click", function () {
+    document.getElementById("bioModal").style.display = "none";
+});
+
+// Close the modal when clicking outside the modal content or pressing the main page button
+document.getElementById("modalOverlay").addEventListener("click", function (e) {
+    if (e.target === this) {
+        document.getElementById("bioModal").style.display = "none";
+    }
+});
+
 
